0312d12692553e2be166ab2d959145b0a7a79d95
Liam Galvin <liam@liam-galvin.co.uk>
2018-12-06 13:24:42 +0000

    update formula

--- aminal.rb
+++ aminal.rb
@@ -1,13 +1,13 @@
 class Aminal < Formula
   homepage "https://github.com/liamg/aminal"
-  url "https://github.com/liamg/aminal/archive/v0.7.11.tar.gz"
-  sha256 "81243638d1478cefde422a2d61e76753743e34baf20ba34cf8723c563432eaa1"
+  url "https://github.com/liamg/aminal/archive/v0.7.12.tar.gz"
+  sha256 "bd424230d2ee2f23c80703208ab22a6ed09d45862557094ef540a219adf25544"
 
   depends_on "go" => :build
 
   def install
     system "make build"
-    bin.install "/tmp/.gobuild/src/github.com/liamg/aminal/aminal" => "aminal"
+    bin.install "./aminal" => "aminal"
   end
 
   test do
