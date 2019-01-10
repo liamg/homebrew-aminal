118b0e1adec2c0f80bdc773470b84073e6332d16
Liam Galvin <liam@liam-galvin.co.uk>
2019-01-10 19:03:10 +0000

    Update to v0.8.5

--- aminal.rb
+++ aminal.rb
@@ -1,12 +1,12 @@
 class Aminal < Formula
   homepage "https://github.com/liamg/aminal"
-  url "https://github.com/liamg/aminal/archive/v0.8.4.tar.gz"
-  sha256 "b861313687f0a195d30417770df20c4b91dec40e6f0b596ec7ebbf168522266b"
+  url "https://github.com/liamg/aminal/archive/v0.8.5.tar.gz"
+  sha256 "d6e54e6b39fb08fad58859e04bdf7282bfe94bf0a37630adaa04cc553c29bdc4"
 
   depends_on "go" => :build
 
   def install
-    system "./build.sh v0.8.4"
+    system "./build.sh v0.8.5"
     bin.install "./aminal" => "aminal"
   end
 
